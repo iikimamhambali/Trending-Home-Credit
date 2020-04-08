aaba9f7725fb650e7161d8ec9123dcd04c42fc78
Imam Hambali <imamhambali@hotmail.co.id>
2020-04-08 14:13:37 +0700

    Configuration package structure

--- app/src/main/java/com/android/homecreditindonesia/ui/MainActivity.kt
+++ app/src/main/java/com/android/homecreditindonesia/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.homecreditindonesia
+package com.android.homecreditindonesia.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.homecreditindonesia.R
 
 class MainActivity : AppCompatActivity() {
 
